4bc4bb09e9ab9f20bae9cbf82e3d51e8f23ddbfb
Brayan Cardona <brayancardona3778@gmail.com>
2024-09-11 18:38:29 -0500

    Improving footer, TODO and other things.

---
+++ services/dataTools.js
@@ -0,0 +1,66 @@
+export let dataTools = [
+    {
+        id: 0,
+        nameProject: 'Lector QR',
+        byDeveloper: 'Brayan Cardona',
+        img: '/logo_qr_proyect.jpeg',
+        link: 'https://brayancardona12.github.io/Generator_QR/'
+
+    },
+    {
+        id: 1,
+        nameProject: 'Make Images',
+        byDeveloper: 'Adobbe Express',
+        img: '/logo-proyect-create-imagen.jpeg',
+        link: 'https://new.express.adobe.com/'
+
+    },
+    {
+        id: 2,
+        nameProject: 'Compress Images',
+        byDeveloper: 'Squoosh',
+        img: '/logo-compress-img.jpeg',
+        link: 'https://squoosh.app/'
+
+    },
+    {
+        id: 3,
+        nameProject: 'Playground',
+        byDeveloper: 'Midudev',
+        img: 'https://cdn.dribbble.com/users/7822692/screenshots/15752919/media/b0e40d6efe62df6b3ae8ab356816b77c.png',
+        link: 'https://codi.link/'
+
+    },
+    {
+        id: 4,
+        nameProject: 'Icons',
+        byDeveloper: 'Tabler Icons | Poland',
+        img: '/logo-icons-proyect.jpeg',
+        link: 'https://tabler.io/icons'
+
+    },
+    {
+        id: 5,
+        nameProject: 'Markdown',
+        byDeveloper: 'Matt Cone | Mexico',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-jQIGGgXonf8tGY2kmw3VtH-rXffnwyagNQ&s',
+        link: 'https://www.markdownguide.org/basic-syntax/'
+
+    },
+    {
+        id: 6,
+        nameProject: 'Coming soon...',
+        byDeveloper: '...........',
+        img: '/logo-coming-soon.jpg',
+        link: '/'
+
+    },
+    {
+        id: 7,
+        nameProject: 'Coming soon...',
+        byDeveloper: '...........',
+        img: '/logo-coming-soon.jpg',
+        link: '/'
+
+    },
+]
